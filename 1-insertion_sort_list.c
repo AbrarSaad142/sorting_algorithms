db4171e6a801235b1cfd6965a1cdb4ed42b41d96
Abrar Saad <abrarsaad3572022@gmail.com>
2024-02-17 03:28:30 +0200

    2-slection_sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - a function that sorts a doubly linked list of integers
+ * in ascending order using the Insertion sort algorithm
+ * @list: we will sort it.
+*/
+void insertion_sort_list(listint_t **list)
+{
+
+}
